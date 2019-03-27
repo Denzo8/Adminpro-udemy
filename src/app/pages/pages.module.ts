67f5b6d180cd9ee80711492f3016a9c016a3b6bc
Josue Joan Martinez Trujillo <e-jjmartinezt@neoris.cxnetworks.net>
2019-03-27 15:21:57 -0700

    Input, Output y view Child

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -7,6 +7,10 @@ import { ProgressComponent } from './progress/progress.component';
 import { Graficas1Component } from './graficas1/graficas1.component';
 import { SharedModule } from '../shared/shared.module';
 import { PAGES_ROUTES } from './pages.routes';
+import { FormsModule } from '@angular/forms';
+import { IncrementadorComponent } from '../components/incrementador/incrementador.component';
+import { ChartsModule } from 'ng2-charts';
+import { GraficoDonaComponent } from '../components/grafico-dona/grafico-dona.component';
 
 
 @NgModule({
@@ -15,17 +19,21 @@ import { PAGES_ROUTES } from './pages.routes';
         DashboardComponent,
         ProgressComponent,
         Graficas1Component,
+        IncrementadorComponent,
+        GraficoDonaComponent
         
     ],
     exports:[
-       
         DashboardComponent,
         ProgressComponent,
         Graficas1Component
     ],
     imports:[
         SharedModule,
-        PAGES_ROUTES
+        PAGES_ROUTES,
+        FormsModule,
+        ChartsModule
+        
     ]
 })
 export class PagesModule{}
